db2db5b5ca121bebb75a34313e7309327b06b71a
Anusha <nssranusha@yahoo.co.in>
2022-09-26 22:05:25 +0530

    Commit : Comments added home page js file

--- assets/js/movie-page.js
+++ assets/js/movie-page.js
@@ -1,39 +1,34 @@
 //api key for fetching the data from the api
 let apikey = "677792fa";
 
-//method to get the URL
+//method to get the URL and get the params from it
 let urlString = window.location.href;
 let url = new URL(urlString);
 let isFavorite = false;
 let searchString = url.searchParams.get("title");
 
-//function to fetch the details when user clicked on a movie
+//method call to fetch the details when user clicked on a movie
 movieDetails();
 
+
+//method to fetch the details when user clicked on a movie by ID
 function movieDetails() {
     fetch(`http://www.omdbapi.com/?apikey=${apikey}&i=${searchString}`)
         .then((response) => response.json())
         .then((data) => {
-
             //add details to the search list
-            details = ` 
-                   
+            details = `  
             <div class="title" > 
                 <span class="movie-title col-12 display-4">${data.Title}</span>
             </div> 
-                       
             <hr class="hr">
-
             <div class="movie-plot font-family">
                 ${data.Plot}
             </div>
-
             <div class="movie-details font-family">
-                
                 <div class="movie-poster">
                     <img src="${data.Poster}" width="200" height="300">
                 </div>
-                
                 <span class="movie-data">
                     <p>
                     Imbdb Ratings
@@ -45,8 +40,7 @@ function movieDetails() {
                     <p><span class="key">Actors: </span> ${data.Actors}</p>
                     <p><span class="key">Genres: </span> ${data.Genre}</p>
                     <p><span class="key">Director: </span> ${data.Director} </p>
-                    <p><span class="key">Released: </span>${data.Released} </p>
-                      
+                    <p><span class="key">Released: </span>${data.Released} </p>     
                     <span id="favSpan">                
                         <button id="favButton" class="btn" onclick=addToFavList(searchString,"movie") >
                             <i id="favIcon" class="fa-solid fa-heart "></i> Add to favorites
@@ -55,6 +49,7 @@ function movieDetails() {
                 </span>
             </div>`
             document.getElementById('movie-details-container').innerHTML = details;
+            //validation to check if the movie is alreday added to list and modify the button appearance
             if(movieExists(data.imdbID)){
                 const favButton= document.getElementById('favSpan');
                 const favSpan =`<span id="favSpan">                
@@ -62,9 +57,7 @@ function movieDetails() {
                     Added to favorites
                 </button>
             </span>`
-
             favButton.innerHTML=favSpan;
-
             }
         });
 }
